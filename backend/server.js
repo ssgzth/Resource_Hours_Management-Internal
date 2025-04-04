b8393d31191f6abc14eb190023d50acafa337225
Suraj Shashikant Ghadge <88426258+ssgzth@users.noreply.github.com>
2025-04-04 09:51:26 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -53,7 +53,9 @@ import "./config/cronjob.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 // Middleware
 app.use(cors());
