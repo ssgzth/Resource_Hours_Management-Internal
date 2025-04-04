1042d7554e7a978be0f78cd9d388167dc1fbda93
Suraj Shashikant Ghadge <88426258+ssgzth@users.noreply.github.com>
2025-04-04 11:48:26 +0530

    Update Project.js
    added new isActive property to the project

--- backend/Models/Project.js
+++ backend/Models/Project.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const projectSchema = new mongoose.Schema({
   name: String,
   businessLine: { type: mongoose.Schema.Types.ObjectId, ref: "BusinessLine" },
+  isActive: { type: Boolean, default: true },
 });
 
 export default mongoose.model("Project", projectSchema);
