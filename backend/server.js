6a345470c732f981d9f982eac6d957a6367bd69a
Suraj Shashikant Ghadge <88426258+ssgzth@users.noreply.github.com>
2025-04-04 09:55:00 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -51,16 +51,16 @@ import "./config/scheduler.js";
 import "./config/cronjob.js";
 
 dotenv.config();
+const PORT = process.env.PORT || 5000;
 
-const app = express();
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on port ${PORT}`);
-});
 
 // Middleware
 app.use(cors());
 app.use(express.json());
-
+const app = express();
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
 // MongoDB Connection
 mongoose
   .connect(process.env.MONGO_URI)
